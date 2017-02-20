d7a507c250282c19a4aff3562a3e1bb66094308b
RJ Burnham <rj@sighthound.com>
2017-02-20 16:28:26 -0500

    Strip down dlib options for iOS - saves ~10MB

--- tools/python/src/dlib.cpp
+++ tools/python/src/dlib.cpp
@@ -3,21 +3,21 @@
 
 #include <boost/python.hpp>
 
-void bind_matrix();
+// void bind_matrix();
 void bind_vector();
-void bind_svm_c_trainer();
-void bind_decision_functions();
-void bind_basic_types();
-void bind_other();
-void bind_svm_rank_trainer();
-void bind_cca();
-void bind_sequence_segmenter();
-void bind_svm_struct();
-void bind_image_classes();
+// void bind_svm_c_trainer();
+// void bind_decision_functions();
+// void bind_basic_types();
+// void bind_other();
+// void bind_svm_rank_trainer();
+// void bind_cca();
+// void bind_sequence_segmenter();
+// void bind_svm_struct();
+// void bind_image_classes();
 void bind_rectangles();
-void bind_object_detection();
+// void bind_object_detection();
 void bind_shape_predictors();
-void bind_correlation_tracker();
+// void bind_correlation_tracker();
 
 #ifndef DLIB_NO_GUI_SUPPORT
 void bind_gui();
@@ -29,21 +29,21 @@ BOOST_PYTHON_MODULE(dlib)
     // since it is full of huge amounts of template clutter.
     boost::python::docstring_options options(true,true,false);
 
-    bind_matrix();
+    // bind_matrix();
     bind_vector();
-    bind_svm_c_trainer();
-    bind_decision_functions();
-    bind_basic_types();
-    bind_other();
-    bind_svm_rank_trainer();
-    bind_cca();
-    bind_sequence_segmenter();
-    bind_svm_struct();
-    bind_image_classes();
+    // bind_svm_c_trainer();
+    // bind_decision_functions();
+    // bind_basic_types();
+    // bind_other();
+    // bind_svm_rank_trainer();
+    // bind_cca();
+    // bind_sequence_segmenter();
+    // bind_svm_struct();
+    // bind_image_classes();
     bind_rectangles();
-    bind_object_detection();
+    // bind_object_detection();
     bind_shape_predictors();
-    bind_correlation_tracker();
+    // bind_correlation_tracker();
 #ifndef DLIB_NO_GUI_SUPPORT
     bind_gui();
 #endif
